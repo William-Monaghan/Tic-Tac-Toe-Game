004e61716eb8eb7601b3ecceb178d89bf27c337b
William Monaghan <113563895+WigglyWorm73@users.noreply.github.com>
2025-11-22 21:32:40 -0600

    Added reset button, filled buttons, message element
    Add Message element to update depending on active user. Filling buttons when clicked, and added reset button with no functionality

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -5,14 +5,43 @@ import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 function TicTacToe() {
+  const [squares,setSquares] = useState(Array(9).fill(""))
+const [winner,setWinner] = useState(false);
+const [active,setActive] = useState("X");
+const [message,setMessage] = useState(`Next Player: ${active}`);
+const winningSets = ["012","345",
+"678","036","147","258","048","246"];
+
+const testWinner = () => {
+
+}
+
+const handleClick = i => {
+  if(squares[i] !== "") return;
+  const nextPlayer = active==="X"?"O":"X";
+
+  const newSquares = [...squares];
+  newSquares[i] = active;
+
+  setActive(nextPlayer);
+  setSquares(newSquares);
+  testWinner();
+  setMessage(`Next Player ${nextPlayer}`)
+}
+
   return (
     <div className="form">
       <h1>Tic-Tac-Toe</h1>
+      <p>{message}</p>
       <div className="grid-layout">
-       {Array.from({ length: 9 }, (_, i) => (
-        <button key={i} className="square"></button>
+       {squares.map((value,i) => (
+        <button key={i} 
+        className="square"
+        onClick={() => handleClick(i)}
+        >{value}</button>
       ))}
       </div>
+      <button id="reset">Reset Game</button>
     </div>
   )
 }
